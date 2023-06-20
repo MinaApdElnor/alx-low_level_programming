71af1387f2d5ea66a681094abb2189adf230e2a0
Mina Apd_Elnor <mina.work250@gmail.com>
2023-06-20 11:00:32 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-/*
- *puts - print function
+
+/**
+ * main - the entry point
  *
- * Description : program prints a sequense 
+ * Description : program prints a sequense
  *
- * Return : return zero if success
+ * Return: return 0 (Success)
  */
-int main()
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0) ;
+	return (0);
 }
