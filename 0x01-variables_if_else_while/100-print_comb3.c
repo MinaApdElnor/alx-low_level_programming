c922b23105dfb7d6376f9c5c563a15a8080e3f99
Mina Apd_Elnor <mina.work250@gmail.com>
2023-06-21 16:55:49 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n, j;
+
+	for (n = 48; n < 57; n++)
+	{
+		for (j = n + 1; j <= 57; j++)
+		{
+			putchar(n);
+			putchar(j);
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+		putchar('\n');
+	return (0);
+
+}
