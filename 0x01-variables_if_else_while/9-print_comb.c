c8c541a27526e80fa75faf8b1fc3ac30b298b948
Mina Apd_Elnor <mina.work250@gmail.com>
2023-06-21 16:26:09 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 49; c <= 57; c++)
+	{
+		putchar(c);
+		if (c < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+	return (0);
+
+}
