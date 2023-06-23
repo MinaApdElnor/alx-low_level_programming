9a4ad7f6ca8af9a29dbc2c3aaaf86bab4bedcbfa
Mina Apd_Elnor <mina.work250@gmail.com>
2023-06-23 05:06:06 -0700

    a

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints 10 times the numbers, from 0 to 14
+ * @n: the number of times
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int k, j;
+
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		for (k = 0 ; k < n ; k++)
+		{
+			for (j = 0; j <= k; j++)
+			{
+				putchar(' ');
+				if (j == k)
+					putchar('\\');
+
+			}
+		putchar('\n');
+		}
+
+
+	}
+
+}
