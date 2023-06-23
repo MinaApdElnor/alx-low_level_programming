605e1d4c4ffcf17074a30c9bca0e1b74191cfcbe
Mina Apd_Elnor <mina.work250@gmail.com>
2023-06-23 05:16:50 -0700

    a

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - prints 10 times the numbers, from 0 to 14
+ * @size: the number of times
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int k, j;
+
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		for (k = 0 ; k < size ; k++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				putchar('#');
+
+			}
+		putchar('\n');
+		}
+
+
+	}
+
+}
