788c8851c72bcbbd9bee45ef629eab983e6d381b
Mina Apd_Elnor <mina.work250@gmail.com>
2023-06-23 08:36:26 -0700

    a

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	long x, maxn;
+	long int number = 612852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+		if (number % x == 0)
+			maxn = number / x;
+	printf("%ld \n", maxn);
+	return (0);
+}
