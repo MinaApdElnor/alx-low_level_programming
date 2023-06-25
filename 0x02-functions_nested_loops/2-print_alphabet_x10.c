b211b4441f2a2ed8fdc2283dd9f0754047dfefd5
Mina Apd_Elnor <mina.work250@gmail.com>
2023-06-24 23:48:31 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - hat prints 10 times the alphabet, in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i < 123; i++)
+			_putchar(i);
+		_putchar('\n');
+
+	}
+
+}
