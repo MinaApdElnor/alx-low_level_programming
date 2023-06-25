3b53fb6a8c9e8bc21aa5bf6531feff8af70fcdf1
Mina Apd_Elnor <mina.work250@gmail.com>
2023-06-25 03:52:14 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -  prints the sign of a number.
+ * @a: first num
+ * @b: second num
+ *
+ * Return: int value
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
