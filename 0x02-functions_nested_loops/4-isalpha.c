037d7da6e2b59aa477920f00699ad9674e303e83
Mina Apd_Elnor <mina.work250@gmail.com>
2023-06-25 01:00:48 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _islower -  checks for lowercase character.
+ * _isalpha -  checks for lowercase character.
  * @c: take a number from the user
  *
  * Return: 1 if c is lowercase , 0 otherwise
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 97 && c < 123) || (c >= 65 && c < 91))
 		return (1);
