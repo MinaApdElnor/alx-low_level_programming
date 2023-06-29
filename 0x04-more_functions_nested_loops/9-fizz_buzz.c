8cc3a893192a0c9d5ff8d500fd1d5ef345b8e94e
Mina Apd_Elnor <mina.work250@gmail.com>
2023-06-29 15:37:20 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,47 +1,42 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - the entry point
  *
  * Return: 0 if Success
  */
 int main(void)
-{
-	int i;
+{	int i;
 
 	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0)
-		{
-			putchar('F');
-			putchar('i');
-			putchar('z');
-			putchar('z');
-			putchar(' ');
+	{	if (i % 3 == 0)
+		{	_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
 		}
 		else if (i % 5 == 0)
-		{
-			putchar('B');
-			putchar('u');
-			putchar('z');
-			putchar('z');
-			putchar(' ');
+		{	_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
 		}
 		else if (i % 15 == 0)
-		{
-			putchar('F');
-			putchar('i');
-			putchar('z');
-			putchar('z');
-			putchar('B');
-			putchar('u');
-			putchar('z');
-			putchar('z');
-			putchar(' ');
+		{	_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
 		}
 		else
-			putchar(' ');
-			putchar(i);
+			_putchar(' ');
+			_putchar(i);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
