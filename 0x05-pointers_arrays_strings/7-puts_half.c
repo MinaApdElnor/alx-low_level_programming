4a5e3999160f3165f8a70fab927d2a23a0fc656c
Mina Apd_Elnor <mina.work250@gmail.com>
2023-07-03 05:53:19 -0700

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,16 @@
 
 void puts_half(char *str)
 {
-	int i, j;
+	int i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-
-	for (j = i / 2; j <= i; j++)
-		_putchar(str[j]);
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 
 
