1f86b6ec747c227a694815c1bbb9b1d69288b904
Mina Apd_Elnor <mina.work250@gmail.com>
2023-07-08 12:48:45 -0700

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strstr - memory  set
+ * @haystack: passing pointer.
+ * @needle: the constant value.
+ *
+ * Return: charpointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, found;
+	char *ptr;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		found = 0;
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+			{
+				found = 1;
+				ptr = &haystack[j];
+				break;
+			}
+		}
+		if (found == 1)
+			break;
+		else
+			continue;
+	}
+	if (found == 1)
+		return (ptr);
+	else
+		return (NULL);
+}
