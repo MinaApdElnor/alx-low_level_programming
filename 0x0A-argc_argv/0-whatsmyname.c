7637378c0f879f9d5b4b841dd0d99cb8e01af9bf
Mina Apd_Elnor <mina.work250@gmail.com>
2023-07-12 17:32:35 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: value of arguments
+ *
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+
+	return (0);
+}
+
+
