4687103c756b533ee2203fb4070c0521ffd6ef09
Mina Apd_Elnor <mina.work250@gmail.com>
2023-07-17 14:49:51 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_h
+
+/**
+ * struct dog - dog struct
+ * @name: name of a dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: kind of dogs in the shop
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
