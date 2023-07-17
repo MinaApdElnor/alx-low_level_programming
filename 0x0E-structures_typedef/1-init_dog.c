bcab31f853905d95871e191e7e92533a99f77468
Mina Apd_Elnor <mina.work250@gmail.com>
2023-07-17 15:01:54 -0700

    a

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ * init_dog - dog struct
+ * @d: the pointer to dog
+ * @name: name of a dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+
+
+
+}
