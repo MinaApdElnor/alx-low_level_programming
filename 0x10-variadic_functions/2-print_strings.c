75c8c14b8e8bd50590381c976ea47fd01b4baa5e
Mina Apd_Elnor <mina.work250@gmail.com>
2023-07-20 04:15:32 -0700

    a

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings- print numbers
+ * @separator: ksj
+ * @n: int ope
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int count;
+	va_list string_list;
+
+	va_start(string_list, n);
+
+	for (count = 0; count < n; count++)
+	{
+		char *each_string = va_arg(string_list, char *);
+
+		if (each_string == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%s", each_string);
+		}
+		if (separator != NULL && count < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(string_list);
+	printf("\n");
+}
