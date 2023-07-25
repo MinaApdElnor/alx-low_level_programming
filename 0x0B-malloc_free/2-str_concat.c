8acb54adac9e8e7c165f61a5960b1f3db835a0e0
Mina Apd_Elnor <mina.work250@gmail.com>
2023-07-25 06:15:19 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,8 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int i;
 
+	if (s1 == NULL || s2 ==  NULL)
+		return (NULL);
 	while (s1[length1] != '\0')
 		length1++;
 	while (s2[length2] != '\0')
